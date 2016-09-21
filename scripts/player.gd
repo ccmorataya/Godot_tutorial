49b404db90a26780ec815f876c5f4ff14f9fa3c9
Cristian Morataya <cris.morataya@gmail.com>
2016-09-21 13:52:01 -0600

    Handling the acceleration in the script

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,10 +2,17 @@
 extends RigidBody2D
 
 export var speed = 200
+export var acceleration = 5
+
+var actual_speed = Vector2(0,0)
 
 var btn_r = Input.is_action_pressed("ui_right")
 var btn_l = Input.is_action_pressed("ui_left")
 
+func moveIt(speed, acceleration_move, delta):
+	actual_speed.x = lerp(actual_speed.x, speed, acceleration_move * delta)
+	set_linear_velocity(Vector2(actual_speed.x, get_linear_velocity().y))
+	
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
@@ -16,10 +23,13 @@ func _fixed_process(delta):
 	btn_l = Input.is_action_pressed("ui_left")
 	
 	if (btn_l):
-		set_linear_velocity(Vector2(-speed,get_linear_velocity().y))
+		moveIt(-speed, acceleration, delta)
+		#set_linear_velocity(Vector2(-speed,get_linear_velocity().y))
 	elif (btn_r):
-		set_linear_velocity(Vector2(speed,get_linear_velocity().y))
+		moveIt(speed, acceleration, delta)
+		#set_linear_velocity(Vector2(speed,get_linear_velocity().y))
 	else:
-		set_linear_velocity(Vector2(0,get_linear_velocity().y))
+		moveIt(0, acceleration, delta)
+		#set_linear_velocity(Vector2(0,get_linear_velocity().y))
 
 
